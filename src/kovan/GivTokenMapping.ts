52b24e9bdb33850a07af194f830939d052e99c2a
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2021-11-23 16:23:17 +0330

    Implement prices query on kovan

--- src/kovan/GivTokenMapping.ts
+++ src/kovan/GivTokenMapping.ts
@@ -1,5 +1,8 @@
 import { Approval, AuthorizationUsed, ChangeMinter, Transfer } from '../../generated/GIV/GIV';
 import { updateBalance } from '../commons/balanceHandler';
+import { Address, log } from '@graphprotocol/graph-ts/index';
+import { uniswapV3Pool } from '../../generated/uniswapV3Pool/uniswapV3Pool';
+import { Price } from '../../generated/schema';
 
 export function handleApproval(event: Approval): void {}
 
@@ -9,4 +12,30 @@ export function handleChangeMinter(event: ChangeMinter): void {}
 
 export function handleTransfer(event: Transfer): void {
   updateBalance(event.params.from.toHex(), event.params.to.toHex(), event.params.value);
+  event.transaction.from.toHex();
+  updateTokenPrice(event.transaction.from.toHex());
+}
+
+function updateTokenPrice(fromAddress: string): void {
+  const uniswapEthGivPoolAddress = Address.fromString('0xa48C26fF05F47a2eEd88C09664de1cb604A21b01');
+  const contract = uniswapV3Pool.bind(uniswapEthGivPoolAddress);
+  const tokensResult = contract.try_getReserves();
+  if (tokensResult.reverted) {
+    return;
+  }
+  const source = 'uniswapV3Pool';
+  const eth = 'ETH';
+  const giv = 'GIV';
+  const priceId = `${source}-${eth}-${giv}`;
+
+  let price = Price.load(priceId);
+  if (!price) {
+    price = new Price(priceId);
+    price.source = source;
+    price.from = eth;
+    price.to = giv;
+  }
+  price.value = tokensResult.value.value0.div(tokensResult.value.value1);
+  price.blockTimeStamp = tokensResult.value.value2;
+  price.save();
 }
--- src/kovan/uniswapV3Mapping.ts
+++ src/kovan/uniswapV3Mapping.ts
@@ -1,31 +1,5 @@
-import { Transfer } from '../../generated/uniswapV3/uniswapV3';
-import { Price } from '../../generated/schema';
-import { uniswapV3Pool } from '../../generated/uniswapV3Pool/uniswapV3Pool';
-import { Address } from '@graphprotocol/graph-ts/index';
-import { log } from '@graphprotocol/graph-ts';
-
-const uniswapEthGivPoolAddress = Address.fromString('0xa48C26fF05F47a2eEd88C09664de1cb604A21b01');
-
-export function handleTransfer(event: Transfer): void {
-  log.error('handleTransfer() has been called', []);
-  const contract = uniswapV3Pool.bind(uniswapEthGivPoolAddress);
-  const tokensResult = contract.try_getReserves();
-  if (tokensResult.reverted) {
-    return;
-  }
-  const source = 'uniswapV3Pool';
-  const eth = 'ETH';
-  const giv = 'GIV';
-  const priceId = `${source}-${eth}-${giv}`;
-
-  let price = Price.load(priceId);
-  if (!price) {
-    price = new Price(priceId);
-    price.source = source;
-    price.from = eth;
-    price.to = giv;
-  }
-  price.value = tokensResult.value.value0.div(tokensResult.value.value1);
-  price.blockTimeStamp = tokensResult.value.value2;
-  price.save();
-}
+// import { Transfer } from '../../generated/uniswapV3/uniswapV3';
+//
+// export function handleTransfer(event: Transfer): void {
+//
+// }
