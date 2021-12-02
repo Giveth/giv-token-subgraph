ec32f420462847a70d5d90c3f325a287ba3ac354
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2021-12-02 19:44:01 +0330

    Implement updating HoneyswapLP in balance
    related #2

--- src/xdai/honeySwapLpMapping.ts
+++ src/xdai/honeySwapLpMapping.ts
@@ -1,38 +1,7 @@
-import { SushiSwapLpToken, Sync } from '../../generated/SushiSwapLpToken/SushiSwapLpToken';
-import { Address } from '@graphprotocol/graph-ts/index';
-import { Price } from '../../generated/schema';
 import { Transfer } from '../../generated/GIV/GIV';
 import { updateBalance } from '../commons/balanceHandler';
-import { SUSHISWAP_LP } from '../helpers/constants';
+import { HONEYSWAP_LP } from '../helpers/constants';
 
 export function handleTransfer(event: Transfer): void {
-  updateBalance(event.params.from.toHex(), event.params.to.toHex(), event.params.value, SUSHISWAP_LP);
-}
-
-export function handleSync(event: Sync): void {
-  updateTokenPrice();
-}
-
-function updateTokenPrice(): void {
-  const sushiSwapContractAddress = Address.fromString('0x8C77Ba1d90C57D584aEEd57bC9B55258B8BE3438');
-  const contract = SushiSwapLpToken.bind(sushiSwapContractAddress);
-  const tokensResult = contract.try_getReserves();
-  if (tokensResult.reverted) {
-    return;
-  }
-  const source = 'sushiSwap';
-  const eth = 'ETH';
-  const giv = 'GIV';
-  const priceId = `${source}-${eth}-${giv}`;
-
-  let price = Price.load(priceId);
-  if (!price) {
-    price = new Price(priceId);
-    price.source = source;
-    price.from = eth;
-    price.to = giv;
-  }
-  price.value = tokensResult.value.value0.div(tokensResult.value.value1);
-  price.blockTimeStamp = tokensResult.value.value2;
-  price.save();
+  updateBalance(event.params.from.toHex(), event.params.to.toHex(), event.params.value, HONEYSWAP_LP);
 }
