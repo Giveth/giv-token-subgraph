a01b90159beb174994e15f9f2342291fc8325aa7
Amin Latifi <a.latifi.al@gmail.com>
2021-12-06 17:08:45 +0330

    replaced static address with value from event in balancerLiquidityMiningMapping.ts

--- src/kovan/balancerLiquidityMiningMapping.ts
+++ src/kovan/balancerLiquidityMiningMapping.ts
@@ -11,29 +11,27 @@ import {
   onRewardPaid,
   onRewardUpdated,
 } from '../commons/unipoolTokenDistributorHandler';
-import { Address } from '@graphprotocol/graph-ts/index';
 import { BALANCER_LIQUIDITY } from '../helpers/constants';
 import { handleBalancerLpStaked, handleBalancerLpWithdrawal } from '../commons/balanceHandler';
-const contractAddress = Address.fromString('0xA14149623488A79ecfd79E63Bb7F5EF2F661A624');
 
 export function handleOwnershipTransferred(event: OwnershipTransferred): void {
-  createUnipoolContractInfoIfNotExists(contractAddress);
+  createUnipoolContractInfoIfNotExists(event.address);
 }
 
 export function handleRewardAdded(event: RewardAdded): void {
-  onRewardAdded(contractAddress);
+  onRewardAdded(event.address);
 }
 
 export function handleRewardPaid(event: RewardPaid): void {
-  onRewardPaid(contractAddress, event.transaction.hash.toHex(), event.params.user.toHex(), BALANCER_LIQUIDITY);
+  onRewardPaid(event.address, event.transaction.hash.toHex(), event.params.user.toHex(), BALANCER_LIQUIDITY);
 }
 
 export function handleStaked(event: Staked): void {
-  onRewardUpdated(contractAddress, event.params.user.toHex(), BALANCER_LIQUIDITY);
+  onRewardUpdated(event.address, event.params.user.toHex(), BALANCER_LIQUIDITY);
   handleBalancerLpStaked(event.params.user.toHex(), event.params.amount);
 }
 
 export function handleWithdrawn(event: Withdrawn): void {
-  onRewardUpdated(contractAddress, event.params.user.toHex(), BALANCER_LIQUIDITY);
+  onRewardUpdated(event.address, event.params.user.toHex(), BALANCER_LIQUIDITY);
   handleBalancerLpWithdrawal(event.params.user.toHex(), event.params.amount);
 }
