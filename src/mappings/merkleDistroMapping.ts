7508b3f718b59a405899a9e10f41e8f9b52a88ad
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2021-12-15 23:43:15 +0330

    Add givDropClaimed to balance and update it in merkleDistroMapping
    related to https://github.com/Giveth/GIVeconomy/issues/292

--- src/mappings/merkleDistroMapping.ts
+++ src/mappings/merkleDistroMapping.ts
@@ -1,8 +1,12 @@
-// import { Claimed, OwnershipTransferred } from '../../generated/MerkleDistro/MerkleDistro';
-// import { updateTokenAllocationDistributor } from '../commons/tokenAllocation';
-//
-// export function handleClaimed(event: Claimed): void {
-//   updateTokenAllocationDistributor(event.transaction.hash.toHex(), 'merkleDistro');
-// }
-//
-// export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+import { Claimed, OwnershipTransferred } from '../../generated/MerkleDistro/MerkleDistro';
+import { Balance } from '../../generated/schema';
+
+export function handleClaimed(event: Claimed): void {
+  const balance = Balance.load(event.params.recipient.toHex());
+  if (balance) {
+    balance.givDropClaimed = true;
+    balance.save();
+  }
+}
+
+export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
