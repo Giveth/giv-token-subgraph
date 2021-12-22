a3bd27c2cca66da1215133a2161b011e55ed4ed8
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2021-12-22 22:11:12 +0330

    Add some comments

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,5 +1,13 @@
+//https://www.notion.so/giveth/Testing-Deployment-218c3f503a04421ba3f51e438f4d6acf
+
+//kovan, Uniswap Reward Token, Incentive Id:
 export const UNISWAP_V3_INCENTIVE_ID = '0x28fb1672f38d284362ad839233ba98ebb3255477338efa2e241a4d566820a160';
+
+//kovan, Uniswap (ETH/GIV)
 export const UNISWAP_V3_POOL_ADDRESS = '0x05f9c5ccc01bc4658516b664be14fb89121c9d15';
+
+// kovan Token (DRGIV2)
 export const MAINNET_GIV_TOKEN_ADDRESS = '0x900d7e4985461ef158f04ca2Ef1aBd136c620DA0';
+
 export const MAINNET_WETH_TOKEN_ADDRESS = '0xd0a1e359811322d97991e03f863a0c30c2cf029c';
 export const SUSHISWAP_ETH_GIV = '0x633799f6d456ACBb20734ecC185B2Ee94995E456';
