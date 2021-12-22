bbd0402e194a2a7bb29f8e912c95d7969f8c8ddf
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2021-12-22 22:44:22 +0330

    WIP update addresses with deployment 7

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,16 +1,16 @@
 //https://www.notion.so/giveth/Testing-Deployment-218c3f503a04421ba3f51e438f4d6acf
 
 //kovan, Uniswap Reward Token, Incentive Id:
-export const UNISWAP_V3_INCENTIVE_ID = '0x7d9c478e7de7f19cf0dcd77b255ce5619af4a2e5fa4f6569912ef2c695d0fd65';
+export const UNISWAP_V3_INCENTIVE_ID = '';
 
-//kovan, Uniswap (ETH/GIV)
-export const UNISWAP_V3_POOL_ADDRESS = '0x77c3a14a9dffaa4b4b94f4f274cdbeb0518be24d';
+//kovan, Uniswap (ETH/GIV) Pool Address
+export const UNISWAP_V3_POOL_ADDRESS = '0x3c2455a3ee0d824941c9329c01a66b86078c3e82';
 
 // kovan Token (DRGIV2)
-export const MAINNET_GIV_TOKEN_ADDRESS = '0x900d7e4985461ef158f04ca2Ef1aBd136c620DA0';
+export const MAINNET_GIV_TOKEN_ADDRESS = '0x29434A25abd94AE882aA883eea81585Aaa5b078D';
 
 // kovan WETH (Uniswap)
 export const MAINNET_WETH_TOKEN_ADDRESS = '0xd0a1e359811322d97991e03f863a0c30c2cf029c';
 
 // xDai SushiSwap (GIV/WETH)
-export const SUSHISWAP_ETH_GIV = '0x0346B748Ce9bdd42995452b5D30b46c296336f07';
+export const SUSHISWAP_ETH_GIV = '0x437B0da7932b21F54488fD80Ee09b519a6f4d8AD';
