2653498432f3db139512f16cfa0b01015b0206e5
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2021-12-23 10:33:27 +0330

    Update addresses for deployment_7, update startBlocks

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,16 +1,16 @@
 //https://www.notion.so/giveth/Testing-Deployment-218c3f503a04421ba3f51e438f4d6acf
 
-//kovan, Uniswap Reward Token, Incentive Id:
+// kovan: Uniswap Reward Token, Incentive Id:
 export const UNISWAP_V3_INCENTIVE_ID = '';
 
-//kovan, Uniswap (ETH/GIV) Pool Address
+// kovan: Uniswap (ETH/GIV) Pool Address
 export const UNISWAP_V3_POOL_ADDRESS = '0x3c2455a3ee0d824941c9329c01a66b86078c3e82';
 
-// kovan Token (DRGIV2)
+// kovan: Token (DRGIV)
 export const MAINNET_GIV_TOKEN_ADDRESS = '0x29434A25abd94AE882aA883eea81585Aaa5b078D';
 
-// kovan WETH (Uniswap)
+// kovan: WETH (Uniswap)
 export const MAINNET_WETH_TOKEN_ADDRESS = '0xd0a1e359811322d97991e03f863a0c30c2cf029c';
 
-// xDai SushiSwap (GIV/WETH)
+// xDai: SushiSwap (GIV/WETH)
 export const SUSHISWAP_ETH_GIV = '0x437B0da7932b21F54488fD80Ee09b519a6f4d8AD';
