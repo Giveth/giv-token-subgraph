521e1191d7aa816008b452f9defcc7e8ca78ea6c
Amin Latifi <a.latifi.al@gmail.com>
2021-12-23 15:50:17 +0330

    Added uniswap staking program incentive id for deployment 7

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,7 +1,7 @@
 //https://www.notion.so/giveth/Testing-Deployment-218c3f503a04421ba3f51e438f4d6acf
 
 // kovan: Uniswap Reward Token, Incentive Id:
-export const UNISWAP_V3_INCENTIVE_ID = '';
+export const UNISWAP_V3_INCENTIVE_ID = '0xe95ce1ce8cc1078b98ea9585ea7e54af8b8f75cd4051267820ab3028af7514fd';
 
 // kovan: Uniswap (ETH/GIV) Pool Address
 export const UNISWAP_V3_POOL_ADDRESS = '0x3c2455a3ee0d824941c9329c01a66b86078c3e82';
