c7e630c4d56b32138f1b5b61f9d08af57f9faf31
Amin Latifi <a.latifi.al@gmail.com>
2022-01-04 12:06:09 +0330

    Fixed empty distributor field on tokenAllocation for mdrkleDrop allocations

--- src/mappings/merkleDistroMapping.ts
+++ src/mappings/merkleDistroMapping.ts
@@ -1,8 +1,12 @@
 import { Claimed, OwnershipTransferred } from '../../generated/MerkleDistro/MerkleDistro';
 import { Balance } from '../../generated/schema';
+import { updateTokenAllocationDistributor } from '../commons/tokenAllocation';
+import { GIVDROP } from '../helpers/constants';
 
 export function handleClaimed(event: Claimed): void {
   const balance = Balance.load(event.params.recipient.toHex());
+  updateTokenAllocationDistributor(event.transaction.hash.toHex(), GIVDROP);
+
   if (balance) {
     balance.givDropClaimed = true;
     balance.save();
