36b7e9cdc96ae3b691076864b550a2fe93de8a04
Amin Latifi <a.latifi.al@gmail.com>
2022-01-10 19:15:22 +0330

    Fixed import issue Added clean script to package.json

--- src/mappings/sushiSwapLpMapping.ts
+++ src/mappings/sushiSwapLpMapping.ts
@@ -1,17 +1,16 @@
-import { Sync } from '../../generated/SushiSwapLpToken/SushiSwapLpToken';
 import { Pair } from '../../generated/schema';
 import { Transfer } from '../../generated/GIV/GIV';
 import { onTransfer } from '../commons/balanceHandler';
 import { SUSHISWAP_LP } from '../helpers/constants';
-import { UniswapV2Pair } from "../../generated/SushiSwapLpToken/UniswapV2Pair";
+import { Sync, UniswapV2Pair } from '../../generated/SushiSwapLpToken/UniswapV2Pair';
 
 export function handleTransfer(event: Transfer): void {
   onTransfer(event.params.from.toHex(), event.params.to.toHex(), event.params.value, SUSHISWAP_LP);
 }
 
 export function handleSync(event: Sync): void {
-  const _reserve0 = event.params._reserve0;
-  const _reserve1 = event.params._reserve1;
+  const reserve0 = event.params.reserve0;
+  const reserve1 = event.params.reserve1;
 
   let pair = Pair.load(event.address.toHex());
   if (!pair) {
@@ -21,7 +20,7 @@ export function handleSync(event: Sync): void {
     pair.token1 = contract.token1().toHex();
   }
 
-  pair.reserve0 = _reserve0;
-  pair.reserve1 = _reserve1;
+  pair.reserve0 = reserve0;
+  pair.reserve1 = reserve1;
   pair.save();
 }
